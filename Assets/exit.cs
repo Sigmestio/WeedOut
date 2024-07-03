83ae18a875265e23362c083d34ecd0be113b7e9e
Wojciech <wkromolski@gmail.com>
2024-07-03 15:01:46 +0200

    you need a key to open the door and escape
    tak wiem, miałem już nie robić

---
+++ Assets/exit.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    
+    public GameObject youescapedPlane;
+    public GameObject youescapedText;
+    public GameObject exitText;
+    
+
+
+    [SerializeField] private bool inReach;
+
+
+    void Start()
+    {
+        inReach = false;
+        youescapedPlane.SetActive(false);
+        youescapedText.SetActive(false);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            exitText.SetActive(true);
+
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            exitText.SetActive(false);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            youescapedPlane.SetActive(true);
+            youescapedText.SetActive(true);
+            
+
+
+
+        }
+
+
+    }
+}
